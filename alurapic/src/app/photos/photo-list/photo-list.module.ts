a3ace7769c46df3729987952a623b8f2fd1bb5f5
Gleidson Teixeira Duarte <gleidson.duarte@pravaler-153.idealinvest.local>
2020-03-28 20:36:34 -0300

    Primeira parte do curso de Angular concluída.

---
+++ alurapic/src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,28 @@
+import { DarkenOnHoverModule } from './../../shared/directives/darken-on-hover/darken-on-hover.module';
+import { SearchComponent } from './search/search.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PhotoModule } from './../photo/photo.module';
+import { CardModule } from './../../shared/components/card/card.module';
+import { PhotoListComponent } from './photo-list.component';
+import { PhotosComponent } from './photos/photos.component';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { FilterByDescription } from './filter-by-description.pipe';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    PhotoModule,
+    CardModule,
+    DarkenOnHoverModule
+  ],
+  declarations: [
+    PhotoListComponent,
+    PhotosComponent,
+    LoadButtonComponent,
+    FilterByDescription,
+    SearchComponent
+  ]
+})
+export class PhotoListModule { }
